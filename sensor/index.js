4b730d19c5caf2c8d2a671b701b1497fea401f52
Greg <fedorov.greg@gmail.com>
2018-07-18 18:43:06 +0300

    Made tessel call remote with a request of readings

--- sensor/index.js
+++ sensor/index.js
@@ -1,27 +1,37 @@
-/*********************************************
-This basic climate example logs a stream
-of temperature and humidity to the console.
-*********************************************/
-
 var tessel = require('tessel');
 var climatelib = require('climate-si7020');
+var request = require('request');
 
 var climate = climatelib.use(tessel.port['A']);
 
 climate.on('ready', function () {
-  console.log('Connected to climate module');
+	console.log('Connected to climate module');
 
-  // Loop forever
-  setImmediate(function loop () {
-    climate.readTemperature('c', function (err, temp) {
-      climate.readHumidity(function (err, humid) {
-      console.log('Degrees:', temp.toFixed(4) + 'C', 'Humidity:', humid.toFixed(4) + '%RH');
-      setTimeout(loop, 300);
-      });
-    });
-  });
+	// Loop forever
+	setImmediate(function loop () {
+		climate.readTemperature('c', function (err, temp) {
+			climate.readHumidity(function (err, humid) {
+			var temperature = temp.toFixed(4);
+			var humidity = humid.toFixed(4);
+			request.post({
+				url:'http://climate.pomp.si/api/readings',
+				body: {
+					"temperature": temperature,
+					"humidity": humidity
+				},
+				json: true
+			}, function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+							console.log(body)
+					}
+				}
+			);
+			setTimeout(loop, 5000);
+			});
+		});
+	});
 });
 
 climate.on('error', function(err) {
-  console.log('error connecting module', err);
+	console.log('error connecting module', err);
 });
